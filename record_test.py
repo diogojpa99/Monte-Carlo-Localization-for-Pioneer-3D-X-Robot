e151cbcc981603a6837a4fb32ee496d0e26df716
PauloCruz <52928501+Cretomilde@users.noreply.github.com>
2022-05-29 14:58:38 +0100

    Create record_test.py

---
+++ record_test.py
@@ -0,0 +1,19 @@
+# !/usr/bin/env python
+
+import rospy
+from std_msgs.msg import String
+from sensor_msgs.msg import LaserScan
+
+global my_data
+
+def callback(data):
+    my_data = data
+    rospy.loginfo("I heard %s", my_data.ranges[500])
+
+def listener_new():
+    rospy.init_node('listener_new', anonymous=True)
+    rospy.Subscriber("scan", LaserScan, callback)
+    rospy.spin()
+
+if __name__ == '__main__':
+    listener_new()
