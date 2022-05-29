9a6d8b692de91ebee355f9d4c5355640df020b5f
PauloCruz <52928501+Cretomilde@users.noreply.github.com>
2022-05-29 15:42:24 +0100

    Create Get_Odom.py

---
+++ Get_Odom.py
@@ -0,0 +1,20 @@
+# !/usr/bin/env python
+
+import rospy
+from std_msgs.msg import String
+from sensor_msgs.msg import LaserScan
+from nav_msgs.msg import Odometry
+
+global my_data
+
+def callback(data):
+    my_data = data
+    rospy.loginfo("I heard %s", my_data.pose.pose.position)
+
+def listener_new():
+    rospy.init_node('listener_new', anonymous=True)
+    rospy.Subscriber("pose", Odometry, callback)
+    rospy.spin()
+
+if __name__ == '__main__':
+    listener_new()
