8a5fc58fa0b577a163b987e165ccca858db80ed6
PauloCruz <52928501+Cretomilde@users.noreply.github.com>
2022-05-29 19:17:48 +0100

    Update Get_Odom.py

--- Get_Odom.py
+++ Get_Odom.py
@@ -10,10 +10,12 @@ global my_data
 def callback(data):
     my_data = data
     rospy.loginfo("I heard %s", my_data.pose.pose.position)
+    #rospy.loginfo("I heard %s", my_data.range[360])
 
 def listener_new():
     rospy.init_node('listener_new', anonymous=True)
     rospy.Subscriber("pose", Odometry, callback)
+    #rospy.Subscriber("scan", LaserScan, callback)
     rospy.spin()
 
 if __name__ == '__main__':
